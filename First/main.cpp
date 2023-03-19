4699f924e68d576db955934f518343d7d70c5aa2
SHIVAM <shivamvashisth020@gmail.com>
2023-03-19 18:32:07 +0530

    first commit

---
+++ First/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string str;
+
+    cout<<"May i know your name "<<endl;
+    getline(cin, str);
+
+    cout<<"Hello "<<str;
+
+    return 0;
+}
