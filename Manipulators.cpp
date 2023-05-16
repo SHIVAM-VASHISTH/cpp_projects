b5dd9cd24a9ef5d170d88e6e9488391ff01755d0
SHIVAM <shivamvashisth020@gmail.com>
2023-05-16 20:02:22 +0530

    WIP

---
+++ Manipulators.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Shivam" << "\n";
+    cout << "Vashisth" << endl;
+    cout << hex << 163 << endl;
+    cout << oct << 163 << endl;
+    cout << dec << 163 << endl;
+    cout << fixed << 125.731;
+    return 0;
+}
